f39bbef68a364c9194eef60a2756df1bd4204f17
root <root@LAPTOP-2VFFP9RP.localdomain>
2020-09-30 01:10:05 +0900

    mod

--- src/editor/clipboard.rs
+++ src/editor/clipboard.rs
@@ -9,8 +9,8 @@ use std::process::Command;
 
 impl Editor {
     pub fn set_clipboard(&mut self, copy_string: &str) {
-        if let Err(err) = self.try_clipboard(&copy_string) {
-            Log::ep("try_clipboard err", err.to_string());
+        if let Err(err) = self.try_set_clipboard(&copy_string) {
+            Log::ep("try_set_clipboard err", err.to_string());
             let result: Result<ClipboardContext, Box<_>> = ClipboardProvider::new();
             match result {
                 Ok(mut ctx) => ctx.set_contents(copy_string.to_string()).unwrap(),
@@ -37,7 +37,7 @@ impl Editor {
             }
         }*/
     }
-    pub fn try_clipboard(&mut self, copy_string: &str) -> anyhow::Result<()> {
+    fn try_set_clipboard(&mut self, copy_string: &str) -> anyhow::Result<()> {
         let mut p = Command::new("powershell.exe").arg("set-clipboard").arg("-Value").arg(copy_string).stdin(process::Stdio::piped()).spawn()?;
         {
             let mut stdin = p.stdin.take().context("take stdin")?;
@@ -48,28 +48,32 @@ impl Editor {
     }
 
     pub fn get_clipboard(&mut self) -> anyhow::Result<String> {
-        let p = Command::new("powershell.exe")
-            .arg("get-clipboard")
-            .stdout(process::Stdio::piped())
-            .spawn()
-            .or_else(|_| Command::new("pbpaste").stdout(process::Stdio::piped()).spawn())
-            .or_else(|_| Command::new("win32yank").arg("-o").stdout(process::Stdio::piped()).spawn())
-            .or_else(|_| Command::new("win32yank.exe").arg("-o").stdout(process::Stdio::piped()).spawn())
-            .or_else(|_| Command::new("xsel").arg("-bo").stdout(process::Stdio::piped()).spawn())
-            .or_else(|_| Command::new("xclip").arg("-o").stdout(process::Stdio::piped()).spawn())?;
+        let result = self.try_get_clipboard();
+
+        match result {
+            Ok(string) => return Ok(string),
+            Err(err) => {
+                Log::ep("try_get_clipboard err", err.to_string());
+                let provider: Result<ClipboardContext, Box<_>> = ClipboardProvider::new();
+                match provider {
+                    Ok(mut ctx) => return Ok(ctx.get_contents().unwrap_or("".to_string())),
+                    Err(_) => {
+                        Log::ep_s("get memory");
+                        return Ok(self.clipboard.clone());
+                    }
+                }
+            }
+        }
+    }
+
+    fn try_get_clipboard(&mut self) -> anyhow::Result<String> {
+        let p = Command::new("powershell.exe").arg("get-clipboard").stdout(process::Stdio::piped()).spawn()?;
         let mut stdout = p.stdout.context("take stdout")?;
         let mut buf = String::new();
         stdout.read_to_string(&mut buf)?;
 
         buf = buf.clone().trim().to_string();
 
-        if buf.len() == 0 {
-            let result: Result<ClipboardContext, Box<_>> = ClipboardProvider::new();
-            if let Ok(mut ctx) = result {
-                buf = ctx.get_contents().unwrap_or("".to_string())
-            }
-            Log::ep("ClipboardContext", buf.clone());
-        }
         Ok(buf)
     }
 }
