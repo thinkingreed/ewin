04facc1b8d3de56605a128d754c58dda7f6f38a2
thinkingreed <thinkingreed.work@gmail.com>
2020-12-27 22:34:29 +0900

    mod

--- src/log.rs
+++ src/log.rs
@@ -3,10 +3,11 @@ use std::fmt::Display;
 
 impl Log {
     pub fn ep<T: Display>(m: &str, v: T) {
+        eprintln!("{}{} {}", Colors::get_default_fg(), format!("{:?}", m), v);
         if cfg!(debug_assertions) {
             eprintln!("{}{} {}", Colors::get_default_fg(), format!("{:?}", m), v);
         } else {
-      
+
             /*
             let debug_mode: &str = ARGS.get("debug_mode").unwrap();
             if debug_mode == "true" {
@@ -15,15 +16,16 @@ impl Log {
         }
     }
     pub fn ep_s(m: &str) {
+        eprintln!("{}{}", Colors::get_default_fg(), m);
         if cfg!(debug_assertions) {
             eprintln!("{}{}", Colors::get_default_fg(), m);
         } else {
-      
+
             /*
-            let debug_mode: &str = ARGS.get("debug_mode").unwrap();
-            if debug_mode == "true" {
-           }
-            */
+             let debug_mode: &str = ARGS.get("debug_mode").unwrap();
+             if debug_mode == "true" {
+            }
+             */
         }
     }
 }
