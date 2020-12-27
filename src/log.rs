25605f9510786b920e0c72ebbd401380368e6ae1
thinkingreed <thinkingreed.work@gmail.com>
2020-12-28 01:42:34 +0900

    mod

--- src/log.rs
+++ src/log.rs
@@ -3,7 +3,7 @@ use std::fmt::Display;
 
 impl Log {
     pub fn ep<T: Display>(m: &str, v: T) {
-        eprintln!("{}{} {}", Colors::get_default_fg(), format!("{:?}", m), v);
+        // eprintln!("{}{} {}", Colors::get_default_fg(), format!("{:?}", m), v);
         if cfg!(debug_assertions) {
             eprintln!("{}{} {}", Colors::get_default_fg(), format!("{:?}", m), v);
         } else {
@@ -16,7 +16,7 @@ impl Log {
         }
     }
     pub fn ep_s(m: &str) {
-        eprintln!("{}{}", Colors::get_default_fg(), m);
+        // eprintln!("{}{}", Colors::get_default_fg(), m);
         if cfg!(debug_assertions) {
             eprintln!("{}{}", Colors::get_default_fg(), m);
         } else {
