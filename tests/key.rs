df3b842202fac66504dfbd5fec1eedb54b4566ae
thinkingreed <thinkingreed.work@gmail.com>
2021-10-30 16:01:35 +0900

    mod

--- tests/key.rs
+++ tests/key.rs
@@ -5,13 +5,14 @@ mod common;
 
 #[test]
 fn test_key_input() {
+    /*
     common::setup();
     let mut term = Terminal::new();
     term.activate(&Args { filenm: "tests/file/key_input.txt".to_string(), ..Args::default() });
 
     eprintln!("111 {:?}", term.curt().editor.buf.text.to_string());
 
-    /*
+
     EvtAct::ctrl_editor(&mut term);
     let mut editor = Editor::new();
 
