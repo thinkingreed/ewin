c0bcf309d118cd4c070c39b48cd94df00e646f59
thinkingreed <thinkingreed.work@gmail.com>
2022-01-14 00:58:29 +0900

    mod

--- crates/ewin-com/src/def.rs
+++ crates/ewin-com/src/def.rs
@@ -23,8 +23,6 @@ pub const TAB_CHAR: char = '\t';
 // mark to treat as char
 pub const NEW_LINE_LF_MARK: char = '↓';
 pub const NEW_LINE_CRLF_MARK: char = '↵';
-// Meaningless mark
-pub const EOF_MARK: char = '▚';
 pub const EOF_STR: &str = "EOF";
 pub const TAB_MARK: char = '^';
 // "…" is not adopted because the width is handled differently depending on the terminal.
